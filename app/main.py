bb60e6bd8f0258abe7c57ec6f3fbf19d8e1aff75
Timotej Králik <timotej.kralik@muziker.com>
2024-09-17 14:15:04 +0200

    Logging errors

--- app/main.py
+++ app/main.py
@@ -3,11 +3,22 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+import logging
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 class UserCreate(BaseModel):
     activities: list
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	logging.error(f"{request}: {exc_str}")
+	content = {'status_code': 10422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 @app.post("/post-req/")
 async def create_user(user_data: UserCreate):
