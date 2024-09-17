e21b8389735c7db480651ccab425e6d053dc60d1
Timotej Králik <timotej.kralik@muziker.com>
2024-09-17 10:58:49 +0200

    Commenting uvicorn in main

--- app/main.py
+++ app/main.py
@@ -16,5 +16,5 @@ async def read_root(data):
     return {"Hello": "world"}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
